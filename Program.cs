2edd4f022f7f1792da70a2f5298dc65e4db29ea8
Brad Reed <shenmusha@yahoo.com>
2021-01-09 11:16:01 -0500

    First attmept registry/login check (C#)

--- Program.cs
+++ Program.cs
@@ -4,9 +4,40 @@ namespace Register_LogIn_System
 {
     class Program
     {
+        static string userName;
+        static string password;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public static void Register()
+        {
+            Console.WriteLine("Please enter new username: ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please enter new password:" );
+            password = Console.ReadLine();
+            Console.WriteLine("Registration complete");
+            Console.WriteLine("---------------------------");
+        }
+        public static void Login()
+        {
+            Console.WriteLine("Please enter username: ");
+            if(userName == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your pasword:");
+                    if(password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login complete");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed wrong password. Restart program");
+                }
+            }else
+            {
+                Console.WriteLine("Login fialed, Wrong username. Please restart");
+            }
         }
     }
 }
